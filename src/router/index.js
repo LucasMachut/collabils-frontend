07f634cad8ef6f5e7e705a4908d313d315797367
LucasMachut <machut.lucas@gmail.com>
2023-03-24 23:28:27 +0100

    delete everything except home page

--- src/router/index.js
+++ src/router/index.js
@@ -1,44 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import CategoriesView from '../views/CategoriesView.vue'
-import SignsView from '../views/SignsView.vue'
-import SignsByCategView from '../views/SignsByCategView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
-  },
-  {
-    path: '/category',
-    name: 'category',
-    component: CategoriesView
-  },
-  {
-    path: '/category',
-    name: 'category',
-    component: CategoriesView
-  },
-  {
-    path: '/category/:id',
-    name: 'signs',
-    component: SignsByCategView
-  },
-  {
-    path: '/signs',
-    name: 'signs',
-    component: SignsView
-  },
-  {
-    path: '/registration',
-    name: 'registration',
-    component: () => import(/* webpackChunkName: "registration" */ '../views/RegistrationView.vue')
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: () => import(/* webpackChunkName: "login" */ '../views/LoginView.vue')
   }
 ]
 
